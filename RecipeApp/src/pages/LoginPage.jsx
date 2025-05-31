5175cf92a9ed17906105d3fbd24715563475cd0f
Jim Kennedy <kennedyomata@gmail.com>
2025-05-31 23:20:30 +0300

    added loging in logic

--- RecipeApp/src/pages/LoginPage.jsx
+++ RecipeApp/src/pages/LoginPage.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImg from "../assets/login-image.jpg";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../components/firebase";
+import { toast } from "react-toastify";
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User Logged in Successfully");
+      toast.success("User Registerd successfully", { position: "top-center" });
+    } catch (err) {
+      console.log(err.message);
+      toast.error(err.message, { position: "top-center" });
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-400">
       {/* Main container: column on mobile, row on large screens */}
       <div className="flex flex-col md:flex-row max-xs:h-screen md:w-full max-w-5xl m-0 xs:m-[6%] sm:m-[10%] md:m-[5%] bg-white rounded-lg shadow-md border border-black/20 md:overflow-hidden">
         {/* Left: Login Form */}
         <div className="w-full md:w-1/2 flex flex-col justify-center">
-          <form className="px-6 py-6 md:px-12 md:py-10">
+          <form onSubmit={handleSubmit} className="px-6 py-6 md:px-12 md:py-10">
             <div className="md:hidden ">
               <img
                 src={loginImg}
@@ -24,7 +42,6 @@ export const LoginPage = () => {
               Today is a new day. It’s your day. You shape it. <br /> Sign in to
               start managing your projects.
             </p>
-
             {/* Email */}
             <label className="block text-sm font-medium mb-1" htmlFor="email">
               Email
@@ -32,11 +49,12 @@ export const LoginPage = () => {
             <input
               id="email"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
               placeholder="Example@email.com"
-              className="w-full px-3 py-2 border border-orange-300  rounded-lg mb-3 focus:outline focus:ring-2 focus:ring-orange-400  bg-white"
+              className="w-full px-3 py-2 border border-orange-300  rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-orange-400  bg-[#f5e7e0]"
             />
-
             {/* Password */}
             <label
               className="block text-sm font-medium mb-1"
@@ -44,14 +62,15 @@ export const LoginPage = () => {
             >
               Password
             </label>
-
             <input
               id="password"
               type="password"
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               min={8}
               placeholder="At least 8 characters"
-              className="w-full px-3 py-2 border border-orange-300  rounded-lg mb-3 focus:outline focus:ring-2 focus:ring-orange-400  bg-white"
+              className="w-full px-3 py-2 border border-orange-300  rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-orange-400  bg-[#f5e7e0]"
             />
             <div className="flex justify-end mb-3 ">
               <a
@@ -61,7 +80,6 @@ export const LoginPage = () => {
                 Forgot Password?
               </a>
             </div>
-
             {/* Sign In Button */}
             <button
               type="submit"
@@ -69,14 +87,14 @@ export const LoginPage = () => {
             >
               Sign in
             </button>
-
             {/* Divider */}
             <div className="flex items-center my-4">
               <div className="flex-grow h-px bg-orange-500"></div>
-              <span className="mx-3 text-gray-400 text-sm">Or</span>
+              <span className="mx-3 text-gray-400 text-sm">
+                Or sign up with
+              </span>
               <div className="flex-grow h-px bg-orange-500"></div>
             </div>
-
             {/* Social Buttons */}
             <div className="flex flex-row md:flex-col gap-3 mb-4">
               <button className="flex items-center gap-2 w-full bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium py-2 rounded-lg border border-gray-300 justify-center">
@@ -84,24 +102,19 @@ export const LoginPage = () => {
                   src="https://img.icons8.com/?size=100&id=17949&format=png&color=000000"
                   style={{ width: 24, height: 24 }}
                 />
-                <p>
-                  <span className="max-md:hidden">Sign in with</span> Google
-                </p>
+                <p>Google</p>
               </button>
               <button className="flex items-center gap-2 w-full bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium py-2 rounded-lg border border-gray-300 justify-center">
                 <img
                   src="https://img.icons8.com/?size=100&id=p62ASPK2Kpqp&format=png&color=000000"
                   style={{ width: 24, height: 24 }}
                 />
-                <p>
-                  <span className="max-md:hidden">Sign in with</span> Facebook
-                </p>
+                <p>Facebook</p>
               </button>
             </div>
-
             {/* Sign up link */}
             <div className="text-center text-sm">
-              Don&apos;t you have an account?{" "}
+              Don't you have an account?{" "}
               <a
                 href="#"
                 className="text-blue-600 font-semibold hover:underline"
