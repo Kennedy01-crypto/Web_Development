fc8c0de3d88dfdd224298e5c783afbfbcaa36d79
Jim Kennedy <kennedyomata@gmail.com>
2025-06-01 01:18:13 +0300

    added dynamic routing to pages based on auth status

--- RecipeApp/src/App.jsx
+++ RecipeApp/src/App.jsx
@@ -1,14 +1,30 @@
 import "./App.css";
 import SignUpPage from "./pages/SignUpPage";
 import { LoginPage } from "./pages/LoginPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { ProfilePage } from "./pages/ProfilePage";
+import { auth } from "./components/firebase";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+
   return (
     <>
       <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/profile" /> : <LoginPage />}
+        />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/log-in" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <ToastContainer />
     </>
